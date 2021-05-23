bfd076690bd16291bef4b3c102d7d8cfbb106b52
gajendra21singh <gajendra.singh@berylsystems.com>
2021-05-24 01:11:11 +0530

    string ruby program

---
+++ ruby/string_4.rb
@@ -0,0 +1,13 @@
+str = "hello world"
+puts str.count('lo')
+puts str.count('lo','o')
+puts str.count('l')
+
+puts "------------------------------------------------------------"
+str = "abcdaaabcdbdjccdjccjbjcbabcdrd"
+puts str.count('b')
+puts str.count('a')
+puts str.count('ba')
+puts str.count('ab')
+puts str.count('aaccd','c')
+puts str.empty?
