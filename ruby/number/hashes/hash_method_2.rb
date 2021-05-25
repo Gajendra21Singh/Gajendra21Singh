32ea9eb69371725e8a9be71c94402819e4f03405
gajendra21singh <gajendra.singh@berylsystems.com>
2021-05-25 11:59:39 +0530

    ruby hash method program

--- ruby/number/hashes/hash_method_2.rb
+++ ruby/number/hashes/hash_method_2.rb
@@ -23,5 +23,43 @@ puts my_hash.empty?
 #fetch
 val1 = product.fetch("product name")
 puts val1
+#has_key?
+puts "key =#{product.has_key?("product name")}"
+a = {"g" => 23, "h" => 25, "x"=>3432, "y"=>3453, "z"=>676}
+p a.has_key?("x")
+p a.has_key?("p")
 
-#values_at
+#has_value?
+puts "value =#{product.has_value?(102)}"
+
+#include? for key
+puts "key =#{product.include?("product price")}"
+
+puts product
+#invert
+pro = product.invert
+puts pro
+
+#key?
+puts pro.key?("motorola")
+
+#member?
+puts pro.member?(9985.85)
+
+#keys and length
+puts "#{pro.keys} and #{pro.length}"
+
+#merge and merge!
+puts pro.merge(product)
+
+#rehash
+x = [ "x", "g" ]
+y = [ "y", "f" ]
+a = { x => 45345, y => 6756 }
+p a[x]
+p x[0] = "h"
+p a[x]
+
+# Using rehash method
+p a.rehash
+p a[x]
