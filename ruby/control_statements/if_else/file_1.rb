7160a93106807622dca6c575bacaa8d5c6e7e08b
gajendra21singh <gajendra.singh@berylsystems.com>
2021-05-27 17:46:30 +0530

    if else program

---
+++ ruby/control_statements/if_else/file_1.rb
@@ -0,0 +1,23 @@
+# if else statement
+puts "Please Enter the marks"
+a = gets.chomp.to_i
+puts "#{a} , #{a.class}"
+=begin
+if a<33
+  puts '"Fail"'
+elsif a>=33 and a <=40
+  puts '"D Grade"'
+elsif a>40 and a<=50
+  puts '"C2 Grade"'
+elsif a>50 && a<=60
+  puts '"C1 Grade"'
+elsif a>60 and a<=70
+  puts "\"B2 Grade\""
+elsif a>70 and a<=80
+  puts '\'B1 Grade\''
+elsif a>80 && a<=90
+  puts "'A2 Grade'"
+elsif a>90 and a<=100
+  puts '"A1 Grade"'
+end
+=end
