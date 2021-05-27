7ce71f4abaa9a62ff658dafaba877b619906afd6
gajendra21singh <gajendra.singh@berylsystems.com>
2021-05-27 23:50:01 +0530

    ternary statement program

---
+++ ruby/control_statements/if_else/test_3.rb
@@ -0,0 +1,12 @@
+#ternary statement
+n1 = 25
+n1%5 == 0 ? (puts "n1 is multiply of 5") : (puts "n1 is not divide by 5")
+
+# find greater number given two number.
+n1 = 15
+n2 = 17
+n1 > n2 ? (puts "n1 is greater = #{n1}") : (puts "n2 is greater = #{n2}")
+
+str_1 = "asdfdsa"
+str_2 = str_1.reverse
+str_1 == str_2 ? (puts "str_1 and str_2 are palindrome") : (puts "str_1 and str_2 not a palindrome") 
