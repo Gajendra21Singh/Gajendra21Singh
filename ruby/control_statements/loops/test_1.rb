61ff2ab9ba772f4c05b748c886e636f8ca5dafb3
gajendra21singh <gajendra.singh@berylsystems.com>
2021-05-28 17:18:26 +0530

    loops program

--- ruby/control_statements/loops/test_1.rb
+++ ruby/control_statements/loops/test_1.rb
@@ -6,3 +6,24 @@ while $i < $num  do
    puts("Inside the loop i = #$i" )
    $i +=1
 end
+
+$n = 5
+count = 1
+loop do
+  break if count == 11
+  puts "#{$n} * #{count} = #{$n*count}"
+  count = count + 1
+end
+
+for i in 1..5
+  puts "#{i}"
+end
+
+num_1 = 5
+count_1 = 1
+while num_1 > 5 and count_1 <= 10
+  puts "#{num_1} * #{count_1} = #{num_1*count_1}"
+  count_1 += 1
+end
+
+puts "Finished"
---
+++ ruby/control_statements/loops/test_2.rb
@@ -0,0 +1,10 @@
+# loops
+a = 5
+while a>0
+  b = 1
+  while b<=5
+    puts "*"*b
+    b = b+1
+  end
+  a = a-1
+end
