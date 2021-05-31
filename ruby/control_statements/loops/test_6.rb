db25bdb810adfe37d8acd5e7cf6b9ce1b5f7ad2c
gajendra21singh <gajendra.singh@berylsystems.com>
2021-05-31 09:23:21 +0530

    do while and loop

---
+++ ruby/control_statements/loops/test_6.rb
@@ -0,0 +1,24 @@
+#loop
+n = 0
+loop do
+  puts "Gajendra Singh"
+  n += 1
+  if n == 10
+    break
+  end
+end
+
+#begin
+
+begin
+    puts "FullStack Developer"
+    n += 2
+end while n <20
+
+$i = 0
+$num = 5
+
+until $i > $num  do
+   puts("Inside the loop i = #$i" )
+   $i +=1;
+end
