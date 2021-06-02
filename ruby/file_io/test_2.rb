1f8b143c15572c8daf8c91df70dd4691494165a6
gajendra21singh <gajendra.singh@berylsystems.com>
2021-06-02 15:54:59 +0530

    w and a+ program

---
+++ ruby/file_io/test_2.rb
@@ -0,0 +1,34 @@
+# file open read and write somethings
+puts "R MODE"
+fileOpen = File.open("file_1.txt","r")
+if fileOpen
+  contant = fileOpen.sysread(35)
+else
+  puts "file can not open"
+end
+fileOpen.close
+puts contant
+
+=begin
+puts "W MODE"
+fileOpen = File.open("file_1.txt","w")
+if fileOpen
+  fileOpen.syswrite("ruby on rails , Reactjs")
+else
+  puts "file can not open"
+end
+fileOpen.close
+=end
+
+#a+ mode
+puts "-------- a+ mode ---------"
+puts "         -------          "
+fileopen = File.open("file_1.txt","a+")
+if fileopen
+  fileopen.gets
+  fileopen.syswrite("Berylsystems private limited")
+  fileopen.gets
+else
+  puts "file not open"
+end
+fileopen.close
