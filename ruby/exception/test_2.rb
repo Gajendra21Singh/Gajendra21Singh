403653cd3c24acb551276d1854a9f1007a7414ab
gajendra21singh <gajendra.singh@berylsystems.com>
2021-06-03 12:35:39 +0530

    adding catch and throw

--- ruby/exception/test_2.rb
+++ ruby/exception/test_2.rb
@@ -8,4 +8,25 @@ def raise_and_rescue
   end
   puts 'After the begin block.'
 end
-raise_and_rescue 
+raise_and_rescue
+
+begin
+ #raise 'A test exception.'
+ puts "no exception is raised"
+rescue Exception => e
+  puts e.message
+  puts e.backtrace.inspect
+else
+   puts "else code will be executed as no exception is raised."
+ensure
+  puts "ensure code will run"
+end
+
+gfg = catch(:divide) do
+  # a code block of catch similar to begin
+    number = rand(2)
+    throw :divide if number == 0
+    number # set gfg = number if
+    # no exception is thrown
+end
+puts gfg
