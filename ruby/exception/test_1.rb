1932ed5c62087e5ef5d9290a2ba9158ba7f977bd
gajendra21singh <gajendra.singh@berylsystems.com>
2021-06-03 12:35:03 +0530

    adding some line

--- ruby/exception/test_1.rb
+++ ruby/exception/test_1.rb
@@ -7,7 +7,24 @@ def raise_exception
   rescue
     puts "catch by rescue"
   ensure
-    puts "this is finally block"  
+    puts "this is finally block"
   end
 end
 raise_exception
+
+begin
+  raise 5/0
+rescue ZeroDivisionError => e
+  puts "Exception Class: #{ e.class.name }"
+  puts "Exception Message: #{ e.message }"
+  puts "Exception Backtrace: #{ e.backtrace }"
+end
+
+begin
+  raise 'Exception'
+rescue Exception => e
+  puts e.message
+  puts e.backtrace.inspect
+ensure
+  puts "The ensure code will always run"
+end 
