7fdc17792ec027383783dda4374c3d7ab6e54265
gajendra21singh <gajendra.singh@berylsystems.com>
2021-06-17 15:41:16 +0530

    schema file

--- rails/library_management/db/schema.rb
+++ rails/library_management/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_092040) do
+ActiveRecord::Schema.define(version: 2021_06_17_095552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2021_06_17_092040) do
     t.string "c_name"
     t.integer "duration"
     t.integer "fee"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "student_book_details", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "course_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
