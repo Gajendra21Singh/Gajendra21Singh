531d4935a8ff831236be1bc88276f4e8b74eb7a7
gajendra21singh <gajendra.singh@berylsystems.com>
2021-06-17 14:58:49 +0530

    creating author and book models

--- rails/library_management/db/schema.rb
+++ rails/library_management/db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_090645) do
+ActiveRecord::Schema.define(version: 2021_06_17_092040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "author_name"
+    t.string "qualification"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "book_name"
+    t.string "book_category"
+    t.integer "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "c_name"
