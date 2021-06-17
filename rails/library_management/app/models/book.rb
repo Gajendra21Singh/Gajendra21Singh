24856fd910c892ad739a04b24a97c8fcbf7473c7
gajendra21singh <gajendra.singh@berylsystems.com>
2021-06-17 15:17:42 +0530

    adding validation in models

--- rails/library_management/app/models/book.rb
+++ rails/library_management/app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :author
+  validates :book_name,:book_category, presence: true
+  validates :book_name, length: { minimum: 4}
+
 end
--- rails/library_management/app/models/course.rb
+++ rails/library_management/app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
   has_many :students
+  validates :c_name,:duration,:fee presence: true
+  validates :c_name,:length: { minimun: 2} 
 end
